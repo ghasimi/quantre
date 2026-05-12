52a1dd4822cc18e2e8f9d0bea80a138770189af6
ghasimi <25824939+ghasimi@users.noreply.github.com>
2026-05-12 16:44:14 -0400

    Format via cargo fmt

--- src/normal.rs
+++ src/normal.rs
@@ -2,68 +2,68 @@
 
 /// Z-score = (x - μ) / σ
 pub fn z_score(x: f64, mu: f64, sigma: f64) -> f64 {
-	return (x - mu) / sigma
+    return (x - mu) / sigma;
 }
 
 /// Probability Density Function (PDF) of Normal Dist.
-pub fn normal_pdf(x:f64, mu: f64, sigma: f64) -> f64 {
-	const PI: f64 = std::f64::consts::PI;
-	let sigma2: f64 = sigma.powi(2);
-	let a: f64 =  1_f64 / (2_f64 * PI * sigma2).sqrt();
-	let b: f64 = -1_f64 * (x - mu).powi(2) / (2_f64 * sigma2);	
-	return a * b.exp()
+pub fn normal_pdf(x: f64, mu: f64, sigma: f64) -> f64 {
+    const PI: f64 = std::f64::consts::PI;
+    let sigma2: f64 = sigma.powi(2);
+    let a: f64 = 1_f64 / (2_f64 * PI * sigma2).sqrt();
+    let b: f64 = -1_f64 * (x - mu).powi(2) / (2_f64 * sigma2);
+    return a * b.exp();
 }
 
 /// Probability Density Function (PDF) of Standard Normal Dist.
-pub fn standard_normal_pdf(z:f64) -> f64 {
-	return  normal_pdf(z, 0., 1.)
+pub fn standard_normal_pdf(z: f64) -> f64 {
+    return normal_pdf(z, 0., 1.);
 }
 
 /// Cumulative Distribution Function (CDF) Approx. of Standard Normal Dist.
-/// 
+///
 /// Source: SR. Bowling et al., 2009
 /// https://hdl.handle.net/10419/188388
-/// 
+///
 /// F(z) ≈ 1 / (1 + e^(-0.07056 * z^3 - 1.5976 * z))
-/// 
-pub fn standard_normal_cdf(z:f64) -> f64 {
-	let a: f64 = -0.07056 * z.powi(3);
-	let b: f64 = -1.5976 * z;
-	return 1_f64 / (1_f64 + (a + b).exp())
+///
+pub fn standard_normal_cdf(z: f64) -> f64 {
+    let a: f64 = -0.07056 * z.powi(3);
+    let b: f64 = -1.5976 * z;
+    return 1_f64 / (1_f64 + (a + b).exp());
 }
 
 /// Cumulative Distribution Function (CDF) Approx. of Normal Dist.
 pub fn normal_cdf(x: f64, mu: f64, sigma: f64) -> f64 {
-	let z: f64 = z_score(x, mu, sigma);
-	return standard_normal_cdf(z);
+    let z: f64 = z_score(x, mu, sigma);
+    return standard_normal_cdf(z);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-	
-	#[test]
-	fn check_z_score(){
-		let z_true: f64 = 2.5;
-		let mu: f64 = -1.;
-		let sigma: f64 = 2.;
-		let x: f64 = z_true * 2. - 1.; // x corresponding to the true z
-		let z_test: f64 = z_score(x, mu, sigma);
-		assert_eq!(z_test, z_true);
-	}
 
-	#[test]
-	fn check_normal_pdf() {
-		let pdf_tue: f64 = 0.00876415; // numpy
-		let pdf_test: f64 = (normal_pdf(4., -1., 2.) * 1e8).round() / 1e8;
-		assert_eq!(pdf_test, pdf_tue);
-	}
+    #[test]
+    fn check_z_score() {
+        let z_true: f64 = 2.5;
+        let mu: f64 = -1.;
+        let sigma: f64 = 2.;
+        let x: f64 = z_true * 2. - 1.; // x corresponding to the true z
+        let z_test: f64 = z_score(x, mu, sigma);
+        assert_eq!(z_test, z_true);
+    }
+
+    #[test]
+    fn check_normal_pdf() {
+        let pdf_tue: f64 = 0.00876415; // numpy
+        let pdf_test: f64 = (normal_pdf(4., -1., 2.) * 1e8).round() / 1e8;
+        assert_eq!(pdf_test, pdf_tue);
+    }
 
     #[test]
     fn check_standard_normal_cdf() {
-		let z: f64 = -1.5;		
-		let cdf_true: f64 = 0.066948; // source approx. (vs numpy 0.066807)
-		let cdf_test: f64 = (standard_normal_cdf(z) * 1e6).round() / 1e6;
+        let z: f64 = -1.5;
+        let cdf_true: f64 = 0.066948; // source approx. (vs numpy 0.066807)
+        let cdf_test: f64 = (standard_normal_cdf(z) * 1e6).round() / 1e6;
         assert_eq!(cdf_test, cdf_true);
     }
 }
