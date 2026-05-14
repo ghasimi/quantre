c8b9d90f797277aefa71b0d79afac99a09c099a2
ghasimi <25824939+ghasimi@users.noreply.github.com>
2026-05-14 13:37:00 -0400

    Simplify normal cdf/pdf function names

--- src/normal.rs
+++ src/normal.rs
@@ -6,17 +6,12 @@ pub fn z_score(x: f64, mu: f64, sigma: f64) -> f64 {
 }
 
 /// Probability Density Function (PDF) of Normal Dist.
-pub fn normal_pdf(x: f64, mu: f64, sigma: f64) -> f64 {
+pub fn pdf(x: f64, mu: f64, sigma: f64) -> f64 {
     const PI: f64 = std::f64::consts::PI;
     let sigma2: f64 = sigma.powi(2);
     let a: f64 = 1_f64 / (2_f64 * PI * sigma2).sqrt();
     let b: f64 = -1_f64 * (x - mu).powi(2) / (2_f64 * sigma2);
     return a * b.exp();
-}
-
-/// Probability Density Function (PDF) of Standard Normal Dist.
-pub fn standard_normal_pdf(z: f64) -> f64 {
-    return normal_pdf(z, 0., 1.);
 }
 
 /// Cumulative Distribution Function (CDF) Approx. of Standard Normal Dist.
@@ -26,14 +21,14 @@ pub fn standard_normal_pdf(z: f64) -> f64 {
 ///
 /// F(z) ≈ 1 / (1 + e^(-0.07056 * z^3 - 1.5976 * z))
 ///
-pub fn standard_normal_cdf(z: f64) -> f64 {
+fn standard_normal_cdf(z: f64) -> f64 {
     let a: f64 = -0.07056 * z.powi(3);
     let b: f64 = -1.5976 * z;
     return 1_f64 / (1_f64 + (a + b).exp());
 }
 
 /// Cumulative Distribution Function (CDF) Approx. of Normal Dist.
-pub fn normal_cdf(x: f64, mu: f64, sigma: f64) -> f64 {
+pub fn cdf(x: f64, mu: f64, sigma: f64) -> f64 {
     let z: f64 = z_score(x, mu, sigma);
     return standard_normal_cdf(z);
 }
@@ -53,9 +48,9 @@ mod tests {
     }
 
     #[test]
-    fn check_normal_pdf() {
+    fn check_pdf() {
         let pdf_tue: f64 = 0.00876415; // numpy
-        let pdf_test: f64 = (normal_pdf(4., -1., 2.) * 1e8).round() / 1e8;
+        let pdf_test: f64 = (pdf(4., -1., 2.) * 1e8).round() / 1e8;
         assert_eq!(pdf_test, pdf_tue);
     }
 
